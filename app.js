ce47bde708749143711087a3251cc27ce85963db
Pablo Papes <pablopapes@gmail.com>
2022-03-28 21:44:02 -0300

    just newline

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { App } = require("@slack/bolt");
 require("dotenv").config();
+
 // Initializes your app with your bot token and signing secret
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
